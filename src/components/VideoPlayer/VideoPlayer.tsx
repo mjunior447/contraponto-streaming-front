96147b5ff79db764133548184699c4db691870b3
Mauricio Antero <mjunior447@gmail.com>
2026-07-04 17:56:37 -0300

    fix: case sensitive directory cache miss on git

--- src/components/VideoPlayer/VideoPlayer.tsx
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -107,7 +107,7 @@ export interface VideoPlayerProps {
  * />
  * ```
  */
-export default function VideoPlayer({ src, className, poster, ...rest }: VideoPlayerProps): ReactNode {
+export function VideoPlayer({ src, className, poster, ...rest }: VideoPlayerProps): ReactNode {
   return (
     <Player.Provider>
       <Container className={`media-default-skin media-default-skin--video ${className ?? ''}`} {...rest}>
